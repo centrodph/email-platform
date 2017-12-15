c3e961d7e2088e181b4603439ef7cdd5f4516226
GerardoMac <centrodph@gmail.com>
2017-12-14 22:15:00 -0300

    client deps

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import { createStore, applyMiddleWare } from 'redux';
+import Provider from 'react-redux';
+import registerServiceWorker from './registerServiceWorker';
+
 registerServiceWorker();
