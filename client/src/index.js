3f9857a7eb90a9214dd9c47fa16a023a68c83a14
GerardoMac <centrodph@gmail.com>
2017-12-14 22:24:37 -0300

    router dom

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import { createStore, applyMiddleWare } from 'redux';
-import Provider from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import ReactDOM from 'react-dom';
+import { Route, BrowserRouter } from 'react-router-dom';
+const store = createStore(applyMiddleware());
 
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <div>Hola Mundo</div>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector('#root')
+);
 registerServiceWorker();
